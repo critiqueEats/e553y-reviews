e7d7df29450d08c3c691e2bd7de1408c37f2e595
e553y <esey.iyasu@gmail.com>
2018-12-21 16:28:02 -0800

    implement end-points  - server end-point  - add model methods  - add packages to packages.json

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const models = require('../database/yelpReviews');
 
 
 const app = express();
@@ -7,5 +9,48 @@ const app = express();
 //setup logging
 app.use(morgan('dev'));
 
+//setup bodyParser
+app.use(bodyParser.json());
+
+//get all reviews for restaurant by id
+app.get('/:restaurantId/reviews', (req, res) => {
+    let restaurantId = req.params.restaurantId;
+    models.getReviewsByRestaurantId(restaurantId, function(err, docs) {
+        if(err) {
+            return res.status(500).json(JSON.stringify({error: "server error"}));
+        }
+        res.status(201);
+        res.json(JSON.stringify(docs));
+    });
+});
+
+//get summary for restaurant by id
+app.get('/:restaurantId/summary', (req, res) => {
+    let restaurantId = req.params.restaurantId;
+    models.getSummaryByRestaurantId(restaurantId, function(err, docs) {
+        if(err) {
+            return res.status(500).json(JSON.stringify({error: "server error"}));
+        }
+        res.status(201);
+        res.json(JSON.stringify(docs));
+    });
+})
+
+//add a new review for restaurant by id
+app.post('/:restaurantId/reviews', (req, res) => {
+    let restaurantId = req.params.restaurantId;
+    let reviewObj = req.body.review;
+    
+    models.addReview(restaurantId,reviewObj, function(err, docs) {
+        if(err) {
+            return res.status(500).json(JSON.stringify({error: "server error"}));
+        }
+        res.status(201);
+        res.json(JSON.stringify(docs));
+    });
+});
+
 let port = 5002;
 const server = app.listen(port, ()=> console.log('Server running on port: ' + server.address().port));
+
+
