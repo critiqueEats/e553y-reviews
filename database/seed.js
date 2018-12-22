423dab7c4831e42f044ca7953f75ca8d719e142d
e553y <esey.iyasu@gmail.com>
2018-12-21 23:04:10 -0800

    bugfix: database seed script  - add random s3 avatar photos to reviews  - add random s3 food pictures to reviews  - code cleanup

--- database/seed.js
+++ database/seed.js
@@ -7,39 +7,34 @@ seedReviews = function(count) {
    while(count--) {
     let user = {};
     user.name = faker.Name.firstName() + ' ' + faker.Name.lastName()[0] + '.',
-    user.photo = faker.Image.avatar(60, 60);
+    user.photo = `https://s3.amazonaws.com/lorem-yelpsum-photos/avatars/images+(${faker.random.number(72)}).jpg`;
     user.city = faker.Address.city();
     user.reviewCount = Math.trunc(Math.random() * 500);
     user.photoCount = Math.trunc(Math.random() * 5);
-    user.elite = [false, false,"'18", "'17", "'16", "'15", "'14"][Math.trunc(Math.random() * 4)];
+    user.elite = [false, false,"'18", "'17", "'16", "'15", "'14"][Math.trunc(Math.random() * 7)];
     
     let reviewObj = {
         user: user,
         restaurantId: Math.trunc(Math.random() * 100),
         date: faker.Date.past(100),
         text: [faker.Lorem.sentences(), faker.Lorem.paragraph(),faker.Lorem.paragraphs()][Math.trunc(Math.random() * 3)],
-        useful: [],
-        funny: [],
-        cool: [],
-        language: ["English", "English", "German", "Spanish"][Math.trunc(Math.random() * 4)],
+        photos: new Array(faker.random.number(4)).fill(0).map(()=> `https://s3.amazonaws.com/lorem-yelpsum-photos/food/images+(${faker.random.number(52)}).jpg`),
+        useful: new Array(faker.random.number(2)).fill(0).map(()=> faker.Name.firstName() + ' ' + faker.Name.lastName()[0] + '.'),
+        funny: new Array(faker.random.number(2)).fill(0).map(()=> faker.Name.firstName() + ' ' + faker.Name.lastName()[0] + '.'),
+        cool: new Array(faker.random.number(2)).fill(0).map(()=> faker.Name.firstName() + ' ' + faker.Name.lastName()[0] + '.'),
+        language: ["English", "English", "English", "Spanish"][Math.trunc(Math.random() * 4)],
         ownerResponse: [null, null, null, faker.Lorem.paragraph()][Math.trunc(Math.random() * 4)],
     };
+    
 
-    let max = Math.trunc(Math.random() * 5);
-    while(max --) {
-        reviewObj.useful.push(faker.Name.firstName() + ' ' + faker.Name.lastName()[0] + '.');
-    }
-     max = Math.trunc(Math.random() * 5);
-    while(max --) {
-        reviewObj.cool.push(faker.Name.firstName() + ' ' + faker.Name.lastName()[0] + '.');
-    }
-     max = Math.trunc(Math.random() * 5);
-    while(max --) {
-        reviewObj.funny.push(faker.Name.firstName() + ' ' + faker.Name.lastName()[0] + '.');
-    }
     reviewsArr.push(reviewObj);
-   }
-   db.Review.create(reviewsArr);
+}
+//delete all documents if they exist
+   db.Review.remove({},function(err) {
+       if(err) {return console.log(err)};
+
+       db.Review.create(reviewsArr);
+   });
    
 }
 seedReviews(1000);
@@ -50,27 +45,35 @@ seedSummary = function() {
     while(restaurantCount--) {
         let summary = {};
         summary.restaurantId = restaurantCount;
-
-        let five = Math.trunc(Math.random() * 100);
+        
+        let five = Math.trunc(Math.random() * 500);
         let four = Math.trunc(Math.random() * 100);
-        let three = Math.trunc(Math.random() * 100);
-        let two = Math.trunc(Math.random() * 100);
-        let one = Math.trunc(Math.random() * 100);
-
+        let three = Math.trunc(Math.random() * 50);
+        let two = Math.trunc(Math.random() * 50);
+        let one = Math.trunc(Math.random() * 50);
+        
         let count = five + four + three + two + one; 
         let average = (five * 5 + four * 4 + three * 3 + two * 2 + one) / count;     
         
-        summary.five = five;
-        summary.four = four;
-        summary.three = three; 
-        summary.two = two;
-        summary.one = one;
+        let stars = {};
+        stars.five = five;
+        stars.four = four;
+        stars.three = three; 
+        stars.two = two;
+        stars.one = one;
+        
         summary.count = count;
         summary.average = average;
-
+        summary.stars = stars;
+        
         summaries.push(summary);
     }
-    db.Summary.create(summaries);
+    //remove all docs if any exist
+    db.Summary.remove({}, function(err) {
+        if(err) {return console.log(err)};
+        
+        db.Summary.create(summaries);
+    })
 }
 
 seedSummary();
