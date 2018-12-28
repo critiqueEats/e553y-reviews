cbbb1dd230a529121a7765f74ab3837fe439909c
e553y <esey.iyasu@gmail.com>
2018-12-28 12:41:04 -0800

    Built MVP reviewListItem
     - made component  stateful to keep track of hover state

--- client/src/components/ReviewListItem/ReviewListItem.jsx
+++ client/src/components/ReviewListItem/ReviewListItem.jsx
@@ -6,40 +6,58 @@ import PhotoPreview from '../PhotoPreview/PhotoPreview.jsx';
 import Vote from '../Vote/Vote.jsx';
 import styles from './styles.css';
 
-const ReviewListItem = ({review})=> {
-    const {user, photos, stars, text, date, cool, funny, useful} = review;
-    return (
-    <div className={styles.container}>
-        <div className={styles.userSidePanel}>
-            <ul>
-                <li>
-                    <UserInfo userInfo={user} />
-                </li>
-                <li>
-                    <Links name={user.name} />
-                </li>
-            </ul>
-        </div>
-        <div className={styles.reviewContentPanel}>
-            <ul>
-                <li className={styles.alignVertical}>
-                    < RatingStars rating={stars} size="medium" />
-                    &nbsp;
-                    <span className={styles.reviewDate}>{new Date(date).toLocaleDateString()}</span>
-                </li>
-                <li>
-                    <p className={styles.reviewText}>{text}</p>
-                </li>
-                <li>
-                    {photos.length? <PhotoPreview photos={photos} /> : ''} 
-                </li>
-                <li>
-                   <Vote  votes={{useful, funny, cool}} />
-                </li>
-            </ul>
-        </div>
-    </div>
-    )
+class ReviewListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hovered: false
+        }
+    }
+    onHoverIn() {
+       this.setState({
+           hovered: true
+       })
+    }
+    onHoverOut() {
+       this.setState({
+           hovered: false
+       })
+    }
+    render(){
+        const {user, photos, stars, text, date, cool, funny, useful} = this.props.review;
+        return (
+            <div className={styles.container} onMouseEnter={this.onHoverIn.bind(this)} onMouseLeave={this.onHoverOut.bind(this)}>
+                <div className={styles.userSidePanel}>
+                    <ul>
+                        <li>
+                            <UserInfo userInfo={user} />
+                        </li>
+                        <li>
+                            <Links name={user.name} show={this.state.hovered} />
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.reviewContentPanel}>
+                    <ul>
+                        <li className={styles.alignVertical}>
+                            < RatingStars rating={stars} size="medium" />
+                            &nbsp;
+                            <span className={styles.reviewDate}>{new Date(date).toLocaleDateString()}</span>
+                        </li>
+                        <li>
+                            <p className={styles.reviewText}>{text}</p>
+                        </li>
+                        <li>
+                            {photos.length? <PhotoPreview photos={photos} /> : ''} 
+                        </li>
+                        <li>
+                        <Vote  votes={{useful, funny, cool}} />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
 }
 
 
