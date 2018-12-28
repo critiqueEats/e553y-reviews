f36a175cf567b71607b0842073d1a9da55150b93
e553y <esey.iyasu@gmail.com>
2018-12-27 22:04:34 -0800

    Refactor LinksComponent
     - takes 'show' prop to set visibility based on prop
     - css rules refactored to utilize Icon component for all
       svg icons
     - href for links set to 'javascript' so that 'visited' pseudoclass
       doesn't activate for all when you click one

--- client/src/components/Links/Links.jsx
+++ client/src/components/Links/Links.jsx
@@ -1,13 +1,15 @@
 const React = require('react');
 const styles = require('./styles.css');
+import Icon from '../Icon/Icon.jsx';
 
-const Links = ({name}) => (
+const Links = ({name, show}) => (
 
-    <div className={styles.container}>
+    <div className={show? styles.container: styles.hiddenContainer}>
         <ul className={styles.linksList}>
             <li className={styles.linkItem}>
-                <a href="#">
-                    <div className={styles.shareIcon}>
+                <a href="javascript:;">
+                    <div className={styles.icon}>
+                        <Icon name="share" width="18" height="18" fill="#0073BB"/>
                     </div>
                     <div className={styles.iconText}>
                         Share review
@@ -15,8 +17,9 @@ const Links = ({name}) => (
                 </a>
             </li>
             <li className={styles.linkItem}>
-                <a href="#">
-                    <div className={styles.embedIcon}>
+                <a href="javascript:;">
+                    <div className={styles.icon}>
+                        <Icon name="embed" width="18" height="18" fill="#0073BB" />
                     </div>
                     <div className={styles.iconText}>
                         Embed review
@@ -24,8 +27,9 @@ const Links = ({name}) => (
                 </a>
             </li>
             <li className={styles.linkItem}>
-                <a href="#">
-                    <div className={styles.complimentIcon}>
+                <a href="javascript:;">
+                    <div className={styles.icon}>
+                        <Icon name="compliment" width="18" height="18" fill="#0073BB" />
                     </div>
                     <div className={styles.iconText}>
                         Compliment
@@ -33,8 +37,9 @@ const Links = ({name}) => (
                 </a>
             </li>
             <li className={styles.linkItem}>
-                <a href="#">
-                    <div className={styles.sendMessageIcon}>
+                <a href="javascript:;">
+                    <div className={styles.icon}>
+                        <Icon name="speech" width="18" height="18" fill="#0073BB" />
                     </div>
                     <div className={styles.iconText}>
                         Send message
@@ -42,8 +47,9 @@ const Links = ({name}) => (
                 </a>
             </li>
             <li className={styles.linkItem}>
-                <a href="#">
-                    <div className={styles.followIcon}>
+                <a href="javascript:;">
+                    <div className={styles.icon}>
+                        <Icon name="following" width="18" height="18" fill="#0073BB"/>
                     </div>
                     <div className={styles.iconText}>
                         Follow {name}
