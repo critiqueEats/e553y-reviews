20b0964bc5b594be4c7ecede42b8c881a1522f8d
e553y <esey.iyasu@gmail.com>
2019-01-01 13:20:28 -0800

    Add default empty avatar for new users

--- database/yelpReviews.js
+++ database/yelpReviews.js
@@ -100,7 +100,7 @@ const searchReviewsByRestaurantId = function(restaurantId, query, callback) {
 const addReview = function(restaurantId, {name, text, stars}, callback) {
     let review = new Review({
         restaurantId: restaurantId,
-        user: {name: name, elite: "'18"},
+        user: {name: name, elite: "'18", photo: "https://s3.amazonaws.com/lorem-yelpsum-photos/avatars/empty_avatar.png"},
         language: 'English',
         text: text,
         stars: stars,
