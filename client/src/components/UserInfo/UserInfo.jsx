a83bfbb0f912f4de06ecafbbdc6d788dc49950b3
e553y <esey.iyasu@gmail.com>
2019-01-02 11:02:44 -0800

    Make label singular if corresponding count is one

--- client/src/components/UserInfo/UserInfo.jsx
+++ client/src/components/UserInfo/UserInfo.jsx
@@ -18,17 +18,17 @@ const UserInfo = ({userInfo}) => {
                     <li className={styles.stats}>
                         <Icon name="friends" width="18" height="18" fill="#f15c00" />
                         &nbsp; <b>{friendCount}</b>
-                        &nbsp;friends
+                        &nbsp;{friendCount === 1 ? 'friend' : 'friends'}
                     </li>
                     <li className={styles.stats}>
                         <Icon name="review" width="18" height="18" fill="#f15c00" />
                         &nbsp;<b>{reviewCount}</b>
-                        &nbsp;reviews
+                        &nbsp;{reviewCount === 1 ? 'review' : 'reviews'}
                     </li>
                     <li className={styles.stats}>
                         <Icon name="camera" width="18" height="18" fill="#f15c00" />
                         &nbsp;<b>{photoCount}</b>
-                        &nbsp; photos
+                        &nbsp;{photoCount === 1 ? 'photo' : 'photos'}
                     </li>
                     <li className={styles.stats}>
                         <a className={styles.eliteStatus} href="javascript:;"><strong>{elite !== 'false' ? "Elite " + elite : ''}</strong></a>
