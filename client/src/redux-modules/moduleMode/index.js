9fa6071482ad33daafb4965523262712257e2ef4
e553y <esey.iyasu@gmail.com>
2019-03-22 19:53:56 -0700

    Add redux-module to handle reviews mode

---
+++ client/src/redux-modules/moduleMode/index.js
@@ -0,0 +1,25 @@
+import types from './types.js';
+
+const initialState = {
+  loading: false,
+  addingReview: false,
+  searchMode: false,
+}
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    case types.MODE_DEFAULT:
+      return Object.assign({},state, {loading:false, addingReview: false, searchMode:false})
+
+    case types.MODE_LOADING:
+      return Object.assign({},state, {loading: true })
+    
+    case types.MODE_SEARCHING:
+      return Object.assign({},state, {searchMode: true })
+    
+    case types.MODE_ADDING_REVIEW:
+      return Object.assign({},state, {addingReview: true })
+    default: 
+      return state;   
+  }
+};
---
+++ client/src/redux-modules/moduleMode/selectors.js
@@ -0,0 +1,3 @@
+export const selectLoading = (state) => state.mode.loading;
+export const selectSearchMode = (state) => state.mode.searchMode;
+export const selectAddingReview = (state) => state.mode.addingReview;
