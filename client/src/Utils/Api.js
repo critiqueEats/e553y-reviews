fd6fe6dcab5c8a615130fadfff8e4335627c592d
e553y <esey.iyasu@gmail.com>
2019-03-30 00:41:34 -0700

    factor out api logic

---
+++ client/src/Utils/Api.js
@@ -0,0 +1,25 @@
+import Axios from 'axios';
+
+const HOST = 'http://127.0.0.1:5002/';
+
+
+
+export const fetchReviews = function (options) {
+  const {sortBy} = options;
+  console.log(sortBy);
+  //throw error if restaurantId is not set
+  if (!global.restaurantId) throw new Error('restaurantId not set');
+
+  return Axios.get(HOST + global.restaurantId + '/reviews', {params:{sortBy}})
+    .then(response => ({reviews: response.data}))
+    .catch(error => { error });
+}
+
+export const searchReviews = function (query) {
+  //throw error if restaurantId is not set
+  if (!global.restaurantId) throw new Error('restaurantId not set');
+
+  return Axios.post(HOST + global.restaurantId + '/search', { query })
+    .then(response => ({reviews: response.data}))
+    .catch(error => { error })
+}
